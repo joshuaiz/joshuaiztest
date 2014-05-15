5f205f4a72058f21dc3e9a0ceccb76ac81234660
Joshua Michaels <joshuaiz@me.com>
2014-05-14 20:46:29 -0500

    cache/updates

---
+++ cache/db/000000/all/fea/e64/feae64974ee2f0230c103f969763bc20.php
@@ -0,0 +1,63 @@
+;tS<?php exit; ?>a:6:{s:10:"last_error";s:0:"";s:10:"last_query";s:49:"SELECT wp_posts.* FROM wp_posts WHERE ID IN (255)";s:11:"last_result";a:1:{i:0;O:8:"stdClass":23:{s:2:"ID";s:3:"255";s:11:"post_author";s:1:"1";s:9:"post_date";s:19:"2014-04-03 08:08:10";s:13:"post_date_gmt";s:19:"2014-04-03 13:08:10";s:12:"post_content";s:1458:"<p><a href="http://joshuaiz.com/wp-content/uploads/2014/04/dcbl_2014-05-25_12.jpg"><img src="http://joshuaiz.com/wp-content/uploads/2014/04/dcbl_2014-05-25_12-231x300.jpg" alt="dcbl_2014-05-25_12" width="231" height="300" class="alignleft size-medium wp-image-243" /></a></p>
+
+<div class="event-description">
+<p><strong>Vizual Records</strong><br>
+& <strong>Your Only Friend</strong><br>
+present:</p>
+
+<h1 style="margin-bottom:0;display:inline-block;">DCBL</h1><span class="dashicons-heart"></span>
+<p class="small">DetroitChicagoBarcelonaLove</p>
+
+
+<h3>Sunday May 25 2014</h3>
+<ul class="nostyle">
+<li>DJ MINX (Women On Wax, Detroit)</li>
+<li>MONTY LUKE (Black Catalogue, Planet E, Detroit)</li>
+<li>IZ&DIZ (Vizual Records, Classic, Chicago)</li>
+<li>TRES MANOS (Your Only Friend, Barcelona)</li>
+<li>AIBY (Aiby & The Noise, Your Only Friend, Barcelona)</li>
+<li>Special Vocal Performance By: MARISSA GUZMAN</li>
+</ul>
+
+<h4>MOTOR CITY WINE</h4>
+<p>1949 Michigan Ave Detroit, MI 48216<br>
+<a href="http://motorcitywine.com/">http://motorcitywine.com/</a><br>
+9pm-2am | 21+</p>
+
+<p>Limited Advance Tickets + More Info:<br>
+<strong>Vizual Records page:</strong> <a href="http://vizualrecords.com/dcbl" title="DCBL" target="_blank">http://vizualrecords.com/dcbl</a><br>
+<strong>RA event page:</strong> <a href="http://www.residentadvisor.net/event.aspx?584362" title="DCBL on RA" target="_blank">http://www.residentadvisor.net/event.aspx?584362</a>
+</p>
+</div>
+";s:10:"post_title";s:32:"DCBL DetroitChicagoBarcelonaLove";s:12:"post_excerpt";s:330:"Vizual Records & Your Only Friend present: DCBL DetroitChicagoBarcelonaLove. Sunday May 25, 2014 at Motor City Wine in Detroit during the Movement Festival. DJs: DJ Minx, Monty Luke, Iz & Diz (with a special vocal performance by Marissa Guzman), Tres Manos and Aiby. <a href="http://joshuaiz:8888/words/dcbl/">Read article →</a>";s:11:"post_status";s:7:"publish";s:14:"comment_status";s:6:"closed";s:11:"ping_status";s:6:"closed";s:13:"post_password";s:0:"";s:9:"post_name";s:4:"dcbl";s:7:"to_ping";s:0:"";s:6:"pinged";s:0:"";s:13:"post_modified";s:19:"2014-05-14 19:29:58";s:17:"post_modified_gmt";s:19:"2014-05-15 00:29:58";s:21:"post_content_filtered";s:1489:"<p><a href="http://joshuaiz.com/wp-content/uploads/2014/04/dcbl_2014-05-25_12.jpg"><img src="http://joshuaiz.com/wp-content/uploads/2014/04/dcbl_2014-05-25_12-231x300.jpg" alt="dcbl_2014-05-25_12" width="231" height="300" class="alignleft size-medium wp-image-243" /></a></p>
+
+<div class="event-description">
+<p><strong>Vizual Records</strong><br>
+& <strong>Your Only Friend</strong><br>
+present:</p>
+
+<h1 style="margin-bottom:0;display:inline-block;">DCBL</h1><span class="dashicons-heart"></span>
+<p class="small">DetroitChicagoBarcelonaLove</p>
+
+
+<h3>Sunday May 25 2014</h3>
+<ul class="nostyle">
+<li>DJ MINX (Women On Wax, Detroit)</li>
+<li>MONTY LUKE (Black Catalogue, Planet E, Detroit)</li>
+<li>IZ&DIZ (Vizual Records, Classic, Chicago)</li>
+<li>TRES MANOS (Your Only Friend, Barcelona)</li>
+<li>AIBY (Aiby & The Noise, Your Only Friend, Barcelona)</li>
+<li>Special Vocal Performance By: MARISSA GUZMAN</li>
+</ul>
+
+<h4>MOTOR CITY WINE</h4>
+<p>1949 Michigan Ave Detroit, MI 48216<br>
+<a href="http://motorcitywine.com/">http://motorcitywine.com/</a><br>
+9pm-2am | 21+</p>
+
+<p>Limited Advance Tickets + More Info:<br>
+<strong>Vizual Records page:</strong> <a href="http://vizualrecords.com/dcbl" title="DCBL" target="_blank">http://vizualrecords.com/dcbl</a><br>
+<strong>RA event page:</strong> <a href="http://www.residentadvisor.net/event.aspx?584362" title="DCBL on RA" target="_blank">http://www.residentadvisor.net/event.aspx?584362</a>
+</p>
+</div>
+";s:11:"post_parent";s:1:"0";s:4:"guid";s:27:"http://joshuaiz:8888/?p=255";s:10:"menu_order";s:1:"0";s:9:"post_type";s:4:"post";s:14:"post_mime_type";s:0:"";s:13:"comment_count";s:1:"0";}}s:8:"col_info";a:23:{i:0;O:8:"stdClass":13:{s:4:"name";s:2:"ID";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:3;s:8:"not_null";i:1;s:11:"primary_key";i:1;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:1;s:4:"blob";i:0;s:4:"type";s:3:"int";s:8:"unsigned";i:1;s:8:"zerofill";i:0;}i:1;O:8:"stdClass":13:{s:4:"name";s:11:"post_author";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:1;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:1;s:10:"unique_key";i:0;s:7:"numeric";i:1;s:4:"blob";i:0;s:4:"type";s:3:"int";s:8:"unsigned";i:1;s:8:"zerofill";i:0;}i:2;O:8:"stdClass":13:{s:4:"name";s:9:"post_date";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:19;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:8:"datetime";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:3;O:8:"stdClass":13:{s:4:"name";s:13:"post_date_gmt";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:19;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:8:"datetime";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:4;O:8:"stdClass":13:{s:4:"name";s:12:"post_content";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:1458;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:1;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:1;s:4:"type";s:4:"blob";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:5;O:8:"stdClass":13:{s:4:"name";s:10:"post_title";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:32;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:1;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:1;s:4:"type";s:4:"blob";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:6;O:8:"stdClass":13:{s:4:"name";s:12:"post_excerpt";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:330;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:1;s:4:"type";s:4:"blob";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:7;O:8:"stdClass":13:{s:4:"name";s:11:"post_status";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:7;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:6:"string";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:8;O:8:"stdClass":13:{s:4:"name";s:14:"comment_status";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:6;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:6:"string";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:9;O:8:"stdClass":13:{s:4:"name";s:11:"ping_status";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:6;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:6:"string";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:10;O:8:"stdClass":13:{s:4:"name";s:13:"post_password";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:0;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:6:"string";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:11;O:8:"stdClass":13:{s:4:"name";s:9:"post_name";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:4;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:1;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:6:"string";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:12;O:8:"stdClass":13:{s:4:"name";s:7:"to_ping";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:0;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:1;s:4:"type";s:4:"blob";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:13;O:8:"stdClass":13:{s:4:"name";s:6:"pinged";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:0;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:1;s:4:"type";s:4:"blob";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:14;O:8:"stdClass":13:{s:4:"name";s:13:"post_modified";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:19;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:8:"datetime";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:15;O:8:"stdClass":13:{s:4:"name";s:17:"post_modified_gmt";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:19;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:8:"datetime";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:16;O:8:"stdClass":13:{s:4:"name";s:21:"post_content_filtered";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:1489;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:1;s:4:"type";s:4:"blob";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:17;O:8:"stdClass":13:{s:4:"name";s:11:"post_parent";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:1;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:1;s:10:"unique_key";i:0;s:7:"numeric";i:1;s:4:"blob";i:0;s:4:"type";s:3:"int";s:8:"unsigned";i:1;s:8:"zerofill";i:0;}i:18;O:8:"stdClass":13:{s:4:"name";s:4:"guid";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:27;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:6:"string";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:19;O:8:"stdClass":13:{s:4:"name";s:10:"menu_order";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:1;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:1;s:4:"blob";i:0;s:4:"type";s:3:"int";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:20;O:8:"stdClass":13:{s:4:"name";s:9:"post_type";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:4;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:1;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:6:"string";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:21;O:8:"stdClass":13:{s:4:"name";s:14:"post_mime_type";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:0;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:0;s:4:"blob";i:0;s:4:"type";s:6:"string";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}i:22;O:8:"stdClass":13:{s:4:"name";s:13:"comment_count";s:5:"table";s:8:"wp_posts";s:3:"def";s:0:"";s:10:"max_length";i:1;s:8:"not_null";i:1;s:11:"primary_key";i:0;s:12:"multiple_key";i:0;s:10:"unique_key";i:0;s:7:"numeric";i:1;s:4:"blob";i:0;s:4:"type";s:3:"int";s:8:"unsigned";i:0;s:8:"zerofill";i:0;}}s:8:"num_rows";i:1;s:10:"return_val";i:1;}
